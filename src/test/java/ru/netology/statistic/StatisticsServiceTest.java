4f5d858c74032d88c91a902c06b9c10053109c5e
Bilalov Danis <hytagyn@gmail.com>
2022-11-13 19:21:37 +0300

    Change POM v.2

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,11 +10,23 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {13, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findElseMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0};
+        long expected = 1;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
